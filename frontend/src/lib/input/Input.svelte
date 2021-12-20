1d6dfc107fd56b60cbe4d795501f8ed2276cde4c
cokejoke <justin@knoter.eu>
2021-12-20 11:28:05 +0100

    :sparkles: Add type number to Input

--- frontend/src/lib/input/Input.svelte
+++ frontend/src/lib/input/Input.svelte
@@ -1,19 +1,41 @@
 <script lang="ts">
   export let width: string = '389px';
   export let height: string = '56px';
-  export let placeholder = '';
-  export let type = 'text';
+  export let placeholder: string = '';
+  export let type: string = 'text';
+  export let border: 'square' | 'roundy' = 'square';
+  export let value: string | number = '';
+  export let min: number = undefined;
+  export let max: number = undefined;
+  export let label: string = undefined;
+  let borderRadius: string = border == 'square' ? '10px' : '32px';
 </script>
 
-<div style="width: {width}; height: {height};" class="input">
-  <input {type} {placeholder} />
+<div style="width: {width}; height: {height};">
+  {#if label}
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label>{label}</label>
+  {/if}
+  <div class="input">
+    <input style="border-radius: {borderRadius};" {type} {placeholder} {value} {min} {max} />
+  </div>
 </div>
 
 <style>
   .input {
     display: flex;
+    height: 100%;
+    width: 100%;
     border-radius: var(--roundy);
     box-shadow: var(--box-shadow);
+  }
+
+  label {
+    margin-left: 30px;
+    font-weight: bold;
+    font-size: 12px;
+    text-transform: uppercase;
+    color: var(--primary-color);
   }
 
   input {
@@ -22,10 +44,17 @@
     height: 100%;
     width: 100%;
     border: none;
-    padding-left: 20px;
-    border-radius: 10px;
+    padding: 0 30px;
     font-size: var(--text-size);
     letter-spacing: var(--letter-space);
+  }
+
+  input[type='number']::-webkit-inner-spin-button,
+  input[type='number']::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    margin: 0;
   }
 
   input::placeholder {
