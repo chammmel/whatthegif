f7c322d5aeab81b9a82b41a011040839bd267708
micmine <micmine4@gmail.com>
2022-04-17 18:12:18 +0200

    Create generate folder

--- generate.sh
+++ generate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p frontend/src/lib/generated/
+
 protoc --plugin=frontend/node_modules/.bin/protoc-gen-ts_proto  \
        --ts_proto_out=frontend/src/lib/generated/ \
        --rust_out=backend/src/generated/ \
