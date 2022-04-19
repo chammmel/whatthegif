39dae9a4d95a8f0a83d237425b49c478296bbcad
cokejoke <justin@knoter.eu>
2022-04-19 21:04:43 +0200

    :sparkles: Add GifButton

---
+++ frontend/src/lib/input/GifButton.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+
+  import { createEventDispatcher } from 'svelte';
+
+  export let background: string;
+  export let color: string = 'white';
+  export let width: string = '389px';
+  export let height: string = '56px';
+  export let href: string = undefined;
+
+  const dispatch = createEventDispatcher();
+
+  function handleClick() {
+    dispatch('click');
+    if (!href) {
+      return;
+    }
+    goto(href);
+  }
+</script>
+
+<button
+  class="gif-button"
+  on:click={handleClick}
+  style="background-color: {background}; color: {color}; width: {width}; height: {height};"
+  ><slot /></button
+>
+
+<style>
+  button {
+    border: none;
+    cursor: pointer;
+    letter-spacing: 4px;
+    font-size: 20px;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+  button:hover {
+    background-blend-mode: multiply;
+    background-image: url('https://i.giphy.com/media/3xz2Bu76X2laXkT636/giphy.webp');
+    background-size: cover;
+    background-position: 50% 50%;
+    transition: 0.5s;
+  }
+</style>
