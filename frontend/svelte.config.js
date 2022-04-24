a5f57ccfa8adbc30a287801bbba1dffa6ce95270
Michael Meier <micmine4@gmail.com>
2022-04-24 11:00:12 +0200

    :construction_worker: Frontend build fix (#7)
    * :sparkles: Add gamestate to protocol

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +8,12 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: node(),
+    vite: {
+      ssr: {
+        noExternal: ['protobufjs']
+      }
+    }
   }
 };
 
